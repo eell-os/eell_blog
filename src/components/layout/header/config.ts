57be3f3014439882791e392b9d5682ec45b1e121
my_eell <73692114+eell-os@users.noreply.github.com>
2024-01-16 22:47:03 +0800

    config.ts

--- src/components/layout/header/config.ts
+++ src/components/layout/header/config.ts
@@ -123,21 +123,6 @@ export const headerMenuConfig: IHeaderMenu[] = [
         path: '/says',
         icon: h(FaSolidComments),
       },
-      {
-        title: '网盘',
-        icon: h(RMixPlanet),
-        path: 'https://pan.eell.pro',
-      },
-      {
-        title: '图床',
-        icon: h(RMixPlanet),
-        path: 'https://image.eell.pro',
-      },
-      {
-        title: '导航',
-        icon: h(RMixPlanet),
-        path: 'https://link.eell.pro',
-      },
     ],
   },
 ]
